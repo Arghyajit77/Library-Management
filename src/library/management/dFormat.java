ba5ddbefa621689457c7039b632282ad2f8ae147
Arghyajit Sarkar <153011594+Arghyajit77@users.noreply.github.com>
2024-09-08 13:04:40 +0530

    Final Turn Over

---
+++ src/library/management/dFormat.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library.management;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ARGHYAJIT
+ */
+class dFormat {
+
+    static String format(Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/library/management/simpleDateFormat.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library.management;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ARGHYAJIT
+ */
+class simpleDateFormat {
+
+    simpleDateFormat(String ddMMyyyy) {
+    }
+
+    String format(Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
